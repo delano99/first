b1aeaef6d3583565499203d6bb38e03944f2cff6
delano <delanonyotue@gmail.com>
2022-03-14 16:10:31 +0100

    begin my web app

---
+++ dist/main.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+let test = "123";
+console.log(test);
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
